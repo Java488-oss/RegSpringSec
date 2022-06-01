5a901a40125a6d82793aeccdfd2f438527f2a139
Verdan <job.mail1358@gmail.com>
2022-06-01 08:48:20 +0600

    Initial commit

---
+++ src/main/java/com/example/RegSpringSec/RegSpringSecApplication.java
@@ -0,0 +1,13 @@
+package com.example.RegSpringSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegSpringSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegSpringSecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RegSpringSec/RegSpringSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RegSpringSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegSpringSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
